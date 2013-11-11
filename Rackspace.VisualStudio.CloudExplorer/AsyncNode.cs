304531e98ca2f49b2417490a4a9edffa7f020c74
Sam Harwell <sam.harwell@rackspace.com>
2013-11-11 16:46:50 -0600

    Base product support

---
+++ Rackspace.VisualStudio.CloudExplorer/AsyncNode.cs
@@ -0,0 +1,91 @@
+﻿namespace Rackspace.VisualStudio.CloudExplorer
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.VSDesigner.ServerExplorer;
+
+    public abstract class AsyncNode : Node
+    {
+        private volatile Task _updateTask;
+        private Node[] _children;
+
+        public AsyncNode()
+        {
+        }
+
+        public bool IsRefreshing
+        {
+            get
+            {
+                return _updateTask != null;
+            }
+        }
+
+        public override sealed string Label
+        {
+            get
+            {
+                string displayText = DisplayText;
+                if (IsRefreshing)
+                    return string.Format("{0} (Refreshing...)", displayText);
+
+                return displayText;
+            }
+
+            set
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        protected abstract string DisplayText
+        {
+            get;
+        }
+
+        public override sealed bool CanEditLabel()
+        {
+            return false;
+        }
+
+        public override sealed Node[] CreateChildren()
+        {
+            lock (this)
+            {
+                Task updateTask = _updateTask;
+                if (updateTask == null)
+                {
+                    Task<Node[]> nodeUpdateTask = CreateChildrenAsync(CancellationToken.None);
+                    if (nodeUpdateTask.IsCompleted)
+                        nodeUpdateTask.ContinueWith(RefreshNodeDisplay, TaskContinuationOptions.ExecuteSynchronously);
+                    else
+                        _updateTask = nodeUpdateTask.ContinueWith(RefreshNodeDisplay);
+                }
+            }
+
+            return _children ?? new Node[0];
+        }
+
+        protected void RefreshNodeDisplay(Task<Node[]> childrenTask)
+        {
+            Node[] oldChildren = Interlocked.Exchange(ref _children, childrenTask.Result);
+
+            INodeSite nodeSite = GetNodeSite();
+            if (nodeSite == null)
+                return;
+
+            if (_updateTask != null)
+            {
+                nodeSite.ResetChildren();
+                foreach (var child in _children)
+                    nodeSite.AddChild(child);
+
+                _updateTask = null;
+                nodeSite.UpdateLabel();
+            }
+        }
+
+        protected abstract Task<Node[]> CreateChildrenAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ Rackspace.VisualStudio.CloudExplorer/CloudProductRootNode.cs
@@ -0,0 +1,41 @@
+﻿namespace Rackspace.VisualStudio.CloudExplorer
+{
+    using System;
+    using Microsoft.VSDesigner.ServerExplorer;
+    using Newtonsoft.Json.Linq;
+    using File = System.IO.File;
+
+    public abstract class CloudProductRootNode : AsyncNode
+    {
+        private static Lazy<JObject> _developerSettings = new Lazy<JObject>(
+            () =>
+            {
+                if (File.Exists(@"C:\.openstack_net"))
+                    return JObject.Parse(File.ReadAllText(@"C:\.openstack_net"));
+
+                return null;
+            }, true);
+
+        protected CloudProductRootNode()
+        {
+        }
+
+        public static JObject DeveloperSettings
+        {
+            get
+            {
+                return _developerSettings.Value;
+            }
+        }
+
+        public override bool CanDeleteNode()
+        {
+            return false;
+        }
+
+        public override sealed int CompareUnique(Node node)
+        {
+            return Label.CompareTo(node.Label);
+        }
+    }
+}
