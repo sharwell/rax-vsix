4a6589a4905404178d03b6acbc9918aab3425f1b
Sam Harwell <sam@tunnelvisionlabs.com>
2013-11-12 09:02:33 -0600

    Add browse component for queue properties

--- Rackspace.VisualStudio.CloudExplorer/Queues/CloudQueueNode.cs
+++ Rackspace.VisualStudio.CloudExplorer/Queues/CloudQueueNode.cs
@@ -1,12 +1,15 @@
 ﻿namespace Rackspace.VisualStudio.CloudExplorer.Queues
 {
     using System;
+    using System.ComponentModel;
     using System.Drawing;
     using System.Threading.Tasks;
     using Microsoft.VSDesigner.ServerExplorer;
     using net.openstack.Core.Domain.Queues;
     using net.openstack.Providers.Rackspace;
+    using Newtonsoft.Json;
     using CancellationToken = System.Threading.CancellationToken;
+    using LocalizableProperties = Microsoft.VisualStudio.Shell.LocalizableProperties;
 
     public class CloudQueueNode : AsyncNode
     {
@@ -58,6 +61,77 @@
         public override bool ConfirmDeletingNode()
         {
             throw new NotImplementedException();
+        }
+
+        public override object GetBrowseComponent()
+        {
+            return new QueueProperties(_provider, _queue);
+        }
+
+        public class QueueProperties : LocalizableProperties, ICustomTypeDescriptor
+        {
+            private readonly CloudQueuesProvider _provider;
+            private readonly CloudQueue _queue;
+
+            public QueueProperties(CloudQueuesProvider provider, CloudQueue queue)
+            {
+                if (provider == null)
+                    throw new ArgumentNullException("provider");
+                if (queue == null)
+                    throw new ArgumentNullException("queue");
+
+                _provider = provider;
+                _queue = queue;
+            }
+
+            [DisplayName("URI")]
+            public string Uri
+            {
+                get
+                {
+                    return _queue.Href.ToString();
+                }
+            }
+
+            [DisplayName("Name")]
+            public string Name
+            {
+                get
+                {
+                    return _queue.Name.Value;
+                }
+            }
+
+            [DisplayName("Region")]
+            public string Region
+            {
+                get
+                {
+                    return _provider.DefaultRegion;
+                }
+            }
+
+            [DisplayName("Metadata")]
+            public string Metadata
+            {
+                get
+                {
+                    if (_queue.Metadata == null)
+                        return string.Empty;
+
+                    return _queue.Metadata.ToString(Formatting.None);
+                }
+            }
+
+            public override string GetClassName()
+            {
+                return "Queue Properties";
+            }
+
+            string ICustomTypeDescriptor.GetComponentName()
+            {
+                return "Cloud Queues";
+            }
         }
     }
 }
