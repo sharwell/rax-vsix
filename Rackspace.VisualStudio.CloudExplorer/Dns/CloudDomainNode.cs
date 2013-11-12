309fbf26c2e9328777cef8d046e1e820f0c0bd1b
Sam Harwell <sam@tunnelvisionlabs.com>
2013-11-12 09:01:36 -0600

    Add browse component for DNS domain properties

--- Rackspace.VisualStudio.CloudExplorer/Dns/CloudDomainNode.cs
+++ Rackspace.VisualStudio.CloudExplorer/Dns/CloudDomainNode.cs
@@ -1,12 +1,14 @@
 ﻿namespace Rackspace.VisualStudio.CloudExplorer.Dns
 {
     using System;
+    using System.ComponentModel;
     using System.Drawing;
     using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.VSDesigner.ServerExplorer;
     using net.openstack.Providers.Rackspace;
     using net.openstack.Providers.Rackspace.Objects.Dns;
+    using LocalizableProperties = Microsoft.VisualStudio.Shell.LocalizableProperties;
 
     public class CloudDomainNode : AsyncNode
     {
@@ -58,6 +60,116 @@
         public override bool ConfirmDeletingNode()
         {
             throw new NotImplementedException();
+        }
+
+        public override object GetBrowseComponent()
+        {
+            return new DomainProperties(_provider, _domain);
+        }
+
+        private class DomainProperties : LocalizableProperties, ICustomTypeDescriptor
+        {
+            private readonly CloudDnsProvider _provider;
+            private readonly DnsDomain _domain;
+
+            public DomainProperties(CloudDnsProvider provider, DnsDomain domain)
+            {
+                if (provider == null)
+                    throw new ArgumentNullException("provider");
+                if (domain == null)
+                    throw new ArgumentNullException("domain");
+
+                _provider = provider;
+                _domain = domain;
+            }
+
+            [DisplayName("ID")]
+            public DomainId Id
+            {
+                get
+                {
+                    return _domain.Id;
+                }
+            }
+
+            [DisplayName("Name")]
+            public string Name
+            {
+                get
+                {
+                    return _domain.Name;
+                }
+            }
+
+            [DisplayName("Account ID")]
+            public string AccountId
+            {
+                get
+                {
+                    return _domain.AccountId;
+                }
+            }
+
+            [DisplayName("Email Address")]
+            public string EmailAddress
+            {
+                get
+                {
+                    return _domain.EmailAddress;
+                }
+            }
+
+            [DisplayName("Comment")]
+            public string Comment
+            {
+                get
+                {
+                    return _domain.Comment;
+                }
+            }
+
+            [DisplayName("Time-To-Live")]
+            public TimeSpan? TimeToLive
+            {
+                get
+                {
+                    return _domain.TimeToLive;
+                }
+            }
+
+            [DisplayName("Created")]
+            public DateTime? Created
+            {
+                get
+                {
+                    if (_domain.Created == null)
+                        return null;
+
+                    return _domain.Created.Value.ToLocalTime().DateTime;
+                }
+            }
+
+            [DisplayName("Last Modified")]
+            public DateTime? Updated
+            {
+                get
+                {
+                    if (_domain.Updated == null)
+                        return null;
+
+                    return _domain.Updated.Value.ToLocalTime().DateTime;
+                }
+            }
+
+            public override string GetClassName()
+            {
+                return "Domain Properties";
+            }
+
+            string ICustomTypeDescriptor.GetComponentName()
+            {
+                return "Cloud DNS";
+            }
         }
     }
 }
