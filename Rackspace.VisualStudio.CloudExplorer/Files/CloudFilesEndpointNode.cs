0d7a08c382afb234e5479b7f29f52a4d35949dd7
Sam Harwell <sam@tunnelvisionlabs.com>
2013-11-27 09:29:29 -0600

    Add Cloud Files containers to server explorer

--- Rackspace.VisualStudio.CloudExplorer/Files/CloudFilesEndpointNode.cs
+++ Rackspace.VisualStudio.CloudExplorer/Files/CloudFilesEndpointNode.cs
@@ -1,9 +1,12 @@
 ﻿namespace Rackspace.VisualStudio.CloudExplorer.Files
 {
+    using System;
+    using System.Collections.Generic;
     using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.VSDesigner.ServerExplorer;
     using net.openstack.Core.Domain;
+    using net.openstack.Providers.Rackspace;
 
     public class CloudFilesEndpointNode : EndpointNode
     {
@@ -12,9 +15,28 @@
         {
         }
 
-        protected override Task<Node[]> CreateChildrenAsync(CancellationToken cancellationToken)
+        protected override async Task<Node[]> CreateChildrenAsync(CancellationToken cancellationToken)
         {
-            return Task.FromResult(new Node[] { new NotImplementedPlaceholderNode() });
+            Tuple<CloudFilesProvider, Container[]> containers = await ListContainersAsync(cancellationToken);
+            return Array.ConvertAll(containers.Item2, i => CreateContainerNode(containers.Item1, i, null));
+        }
+
+        private CloudFilesContainerNode CreateContainerNode(CloudFilesProvider provider, Container container, ContainerCDN containerCdn)
+        {
+            return new CloudFilesContainerNode(provider, container, containerCdn);
+        }
+
+        private async Task<Tuple<CloudFilesProvider, Container[]>> ListContainersAsync(CancellationToken cancellationToken)
+        {
+            CloudFilesProvider provider = CreateProvider();
+            List<Container> containers = new List<Container>();
+            containers.AddRange(await Task.Run(() => provider.ListContainers()));
+            return Tuple.Create(provider, containers.ToArray());
+        }
+
+        private CloudFilesProvider CreateProvider()
+        {
+            return new CloudFilesProvider(Identity, Endpoint.Region, null, null);
         }
     }
 }
