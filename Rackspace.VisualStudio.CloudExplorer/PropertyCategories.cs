effede6cac2963e826d03c483c09138760d9f535
Sam Harwell <sam.harwell@rackspace.com>
2014-03-19 14:57:53 -0500

    Initial Server Explorer support for Cloud Auto Scale

--- Rackspace.VisualStudio.CloudExplorer/PropertyCategories.cs
+++ Rackspace.VisualStudio.CloudExplorer/PropertyCategories.cs
@@ -4,5 +4,9 @@
     {
         public const string Advanced = "Advanced";
         public const string Identity = "Identity";
+
+        public const string AutoScaleState = "State";
+        public const string AutoScaleGroupConfiguration = "Group Configuration";
+        public const string AutoScaleLaunchConfiguration = "Launch Configuration";
     }
 }
