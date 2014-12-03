215240a56b948bf144c58812f6d543eae4765064
Sam Harwell <sam.harwell@rackspace.com>
2014-12-03 16:07:19 -0600

    Updated account manager window view models and styling

---
+++ Rackspace.VisualStudio.CloudExplorer/AccountManager/AccountViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Rackspace.VisualStudio.CloudExplorer.AccountManager
+{
+    using System.ComponentModel;
+
+    public class AccountViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, e);
+        }
+    }
+}
---
+++ Rackspace.VisualStudio.CloudExplorer/AccountManager/RegionViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Rackspace.VisualStudio.CloudExplorer.AccountManager
+{
+    using System.ComponentModel;
+
+    public class RegionViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, e);
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
